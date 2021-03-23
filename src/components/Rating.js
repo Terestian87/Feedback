0d8f5c2286a3dee1969cc48eb4f9483a0585800b
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-03-23 18:21:46 +0100

    Cleaned up code

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -10,25 +10,25 @@ const Rating = () => {
         <div className="scale-div">
             <div className="box">
                 <div className="rating">
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate1" />
+                    <input onClick={handleVote} value={1} className="radio" type="radio" name="rating" id="rate1" />
                     <label htmlFor="rate1">1</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate2" />
+                    <input onClick={handleVote} value={2} className="radio" type="radio" name="rating" id="rate2" />
                     <label htmlFor="rate2">2</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate3" />
+                    <input onClick={handleVote} value={3} className="radio" type="radio" name="rating" id="rate3" />
                     <label htmlFor="rate3">3</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate4" />
+                    <input onClick={handleVote} value={4} className="radio" type="radio" name="rating" id="rate4" />
                     <label htmlFor="rate4">4</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate5" />
+                    <input onClick={handleVote} value={5} className="radio" type="radio" name="rating" id="rate5" />
                     <label htmlFor="rate5">5</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate6" />
+                    <input onClick={handleVote} value={6} className="radio" type="radio" name="rating" id="rate6" />
                     <label htmlFor="rate6">6</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate7" />
+                    <input onClick={handleVote} value={7} className="radio" type="radio" name="rating" id="rate7" />
                     <label htmlFor="rate7">7</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate8" />
+                    <input onClick={handleVote} value={8} className="radio" type="radio" name="rating" id="rate8" />
                     <label htmlFor="rate8">8</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate9" />
+                    <input onClick={handleVote} value={9} className="radio" type="radio" name="rating" id="rate9" />
                     <label htmlFor="rate9">9</label>
-                    <input onClick={handleVote} className="radio" type="radio" name="rating" id="rate10" />
+                    <input onClick={handleVote} value={10} className="radio" type="radio" name="rating" id="rate10" />
                     <label htmlFor="rate10">10</label>
                 </div>
             </div>
