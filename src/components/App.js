7a2698f0dedd496e55fa77979be81a4018e25a33
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-04-10 15:49:35 +0200

    Moved answers handling from state to database with idb

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,65 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { openDB, deleteDB, wrap, unwrap } from 'idb';
+import { openDB } from 'idb';
 // components import
 import Footer from './Footer'
 import Navbar from './Navbar';
 import Users from './Users';
 
 //style impoort
-import '../styles/app.css'
+import '../styles/app.css';
 
+const dbName = 'honesto'
 
+const getStore = async (storeName, cb) => {
+    const db = openDB(dbName, 1, {
+        upgrade(db) {
+            if (!db.objectStoreNames.contains(storeName)) {
+                db.createObjectStore("feedback");
+            }
+        }
+    })
+
+    const tx = (await db).transaction(storeName, 'readwrite')
+    const store = await tx.objectStore(storeName)
+
+    if (cb) {
+        cb(store)
+    }
+}
+
+// const addFeedback = async (feedback, id) => {
+//     await getStore('feedback', () => {
+//         await store.put(feedback, id)
+//     })
+// }
+
+// export const useFeedbacks = () => {
+//     const [feedbacks, setFeedbacks] = React.useState({})
+//     const { getStore } = React.useContext(AppContext)
+
+//     React.useEffect(() => {
+//       getStore('feedback', async (store) => {
+//         let cursor = await store.openCursor();
+//         const newFeedbacks = {}
+
+//         while (cursor) {
+//           const key = cursor.key as string
+//           newFeedbacks[key] = cursor.value
+//           cursor = await cursor.continue();
+//         }
+
+//         setFeedbacks(newFeedbacks)
+//       })
+//     }, [])
+
+//     return feedbacks
+//   }
 
 const App = () => {
+
+
     return (
         <div className="app-div">
 
@@ -18,7 +67,7 @@ const App = () => {
                 <Navbar />
                 <Switch>
                     <Route path="/" exact>
-                        <Users />
+                        <Users getStore={getStore} />
                     </Route>
                     <Route path='*'>
                         <div>404 not found</div>
--- src/components/Users.js
+++ src/components/Users.js
@@ -4,20 +4,20 @@ import Rating from './Rating'
 import Text from './Text'
 import Choice from './Choice'
 //functional component
-const Users = () => {
+
+const Users = ({ getStore }) => {
     const [users, setUsers] = useState([]);
     const [questions, setQuestions] = useState([]);
     const [questionNum, setQuestionNum] = useState(0);
-    const [answers, setAnswers] = useState([]);
     const [chosenId, setChosenId] = useState(null);
-    const [completed, setCompleted] = useState({})
+    // const [completed, setCompleted] = useState(false);
+    const [feedbackData, setFeedbackData] = useState({})
+
     //choice handler
     const [choice, setChoice] = useState('')
     const handleChoice = (e) => {
         setChoice(e.target.value)
     }
-
-
 
     //Fetch data in asyn
     const fetchUserData = async () => {
@@ -41,36 +41,86 @@ const Users = () => {
         fetchQuestions()
     }, [])
 
+    useEffect(() => {
+        getStore('feedback', async (store) => {
+            let cursor = await store.openCursor();
+            const newFeedbacks = {}
+
+            while (cursor) {
+                const { key } = cursor
+                newFeedbacks[key] = cursor.value
+                cursor = await cursor.continue();
+            }
+            setFeedbackData(newFeedbacks)
+        })
+    }, [getStore]) // aggiungi completed
+
+    useEffect(() => {
+        if (!chosenId) {
+            return
+        }
+
+        getStore('feedback', async (store) => {
+            const currentFeedback = await store.get(chosenId)
+            const { answers } = currentFeedback
+            if (answers[questionNum] === null) {
+                return
+            }
+            setChoice(answers[questionNum])
+        })
+    }, [questionNum, chosenId])
+
+
     //conditional rendering on click
     const handleFirstClick = (id) => {
         setChosenId(id)
+
+        getStore('feedback', async store => {
+            const currentFeedback = await store.get(id)
+            if (!currentFeedback) {
+                await store.put({
+                    isSubmitted: false,
+                    answers: [...Array(questions.length)].map(() => null),
+                }, id)
+            }
+        })
     }
 
-    const handleSetAnswers = (arr, value, index) => {
-        const updatedArr = arr.slice();
-        updatedArr[index] = value;
-        setAnswers(updatedArr)
+    const handleSetAnswers = (value, index) => {
+        getStore('feedback', async (store) => {
+            const currentFeedback = await store.get(chosenId)  // data[id]
+            const { answers, isSubmitted } = currentFeedback
+            answers[index] = value
+            await store.put({
+                isSubmitted,
+                answers
+            }, chosenId)
+        })
+
         setChoice('')
     }
 
     //change it to boolean
-    const handleCompletedFeedback = (user, answers) => {
-        const paired = {
-            id: user,
-            answers: [answers]
-        }
+    const handleCompletedFeedback = (user) => {
+        // const paired = {
+        //     id: user,
+        //     answers: [answers]
+        // }
         // const defaultNum = questionNum - 8
-        setCompleted(paired)
-        console.log(paired)
-        console.log(completed)
+        // setCompleted(paired)
+        // getStore('feedback', store => {
+
+        //     await store.put({
+        //         issubmitted: true,
+        //         answers: paired
+        //     })
+        // })
     }
-
-
 
     const handleNext = () => {
         if (questionNum === questions.length - 1) {
-            handleCompletedFeedback(chosenId, answers)
-            handleSetAnswers(answers, choice, questionNum)
+            handleCompletedFeedback(chosenId)
+            handleSetAnswers(choice, questionNum)
             const newQuestion = 0
             setQuestionNum(newQuestion)
             const defaultId = null
@@ -78,7 +128,7 @@ const Users = () => {
             return
         }
         if (questions[questionNum].required === true && choice === '') { return }
-        handleSetAnswers(answers, choice, questionNum)
+        handleSetAnswers(choice, questionNum)
         const newQuestion = questionNum + 1
         setQuestionNum(newQuestion)
     }
@@ -96,7 +146,6 @@ const Users = () => {
         setChoice(null)
         handleNext()
     }
-
 
     return (
         <>
@@ -121,7 +170,7 @@ const Users = () => {
                 <div className="question-div">
                     <h2 className="question-label">{questions[questionNum].label}</h2>
                     <div className="feedback-container">
-                        {questions[questionNum].type === 'scale' && <Rating handleChoice={handleChoice} />}
+                        {questions[questionNum].type === 'scale' && <Rating handleChoice={handleChoice} choice={choice} />}
                         {questions[questionNum].type === 'multipleChoice' && <Choice data={questions[questionNum]} handleChoice={handleChoice} />}
                         {questions[questionNum].type === 'text' && <Text choice={choice} handleChoice={handleChoice} />}
                     </div>
@@ -144,3 +193,7 @@ export default Users
 
 
 
+//adattare le risposte aglle altre due tipi di domande
+//impsiotare is submitted da non fare ricvedfere gli user completi nella lsit ainiziale in modo che a seconda del true o false compaia un bottone diverso
+
+//ultima pagina mostrare gli utienti per cui issubmitted è false filtrando quelli gia completi
