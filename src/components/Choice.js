da6880981bee07de9951bf544a5325d92b3aea3a
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-04-10 19:02:14 +0200

    changed button to radio input

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,21 +1,27 @@
 import React from 'react'
 
 
-const Choice = ({ data, handleChoice }) => {
+const Choice = ({ data, handleChoice, choice }) => {
 
     return (
         <>
             {data.options.map(({ value, label }) => {
                 return (
-                    <button
-                        // onClick={handleChoice}
-                        className="answer-btn"
-                        key={value}
-                        value={value}
-                        onClick={handleChoice}
-                    >
-                        {label}
-                    </button>
+                    // <button
+                    //     // onClick={handleChoice}
+                    //     className="answer-btn"
+                    //     key={value}
+                    //     value={value}
+                    //     onClick={handleChoice}
+                    //     checed={choice === value}
+                    // >
+                    //     {label}
+                    // </button>
+                    <div className="choice-div">
+                        <input onClick={handleChoice} value={value} className="radio-choice" type="radio" name="rating" id={`rate${value}`} cheched={choice === value} />
+                        <label htmlFor={`rate${value}`}>{label} click me</label>
+                    </div>
+
                 )
             })}
         </>
