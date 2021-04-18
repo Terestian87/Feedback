7813459f869e19afe31b366e93e9ed5d103a91b8
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-04-18 13:10:30 +0200

    added feedback link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <div className="nav-right">
                     <ul>
                         <li><Link className="nav-link" to="/">Share Feedback</Link></li>
-                        <li>altra pagina</li>
+                        <li><Link className="nav-link" to="my-feedback">Completed Feedbacks</Link></li>
                         <li>altra pagina ancora</li>
                     </ul>
                 </div>
