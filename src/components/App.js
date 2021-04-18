78840bcb961fb0dd9aca89cc05a195995452f0b8
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-04-18 13:19:49 +0200

    cleaned code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import { openDB, deleteDB, wrap, unwrap } from 'idb';
 import { openDB } from 'idb';
 // components import
 import Footer from './Footer'
 import Navbar from './Navbar';
 import Users from './Users';
-
+import Feedbacks from './Feedbacks'
 //style impoort
 import '../styles/app.css';
 
@@ -20,54 +19,25 @@ const getStore = async (storeName, cb) => {
             }
         }
     })
-
     const tx = (await db).transaction(storeName, 'readwrite')
     const store = await tx.objectStore(storeName)
-
     if (cb) {
         cb(store)
     }
-}
-
-// const addFeedback = async (feedback, id) => {
-//     await getStore('feedback', () => {
-//         await store.put(feedback, id)
-//     })
-// }
-
-// export const useFeedbacks = () => {
-//     const [feedbacks, setFeedbacks] = React.useState({})
-//     const { getStore } = React.useContext(AppContext)
-
-//     React.useEffect(() => {
-//       getStore('feedback', async (store) => {
-//         let cursor = await store.openCursor();
-//         const newFeedbacks = {}
-
-//         while (cursor) {
-//           const key = cursor.key as string
-//           newFeedbacks[key] = cursor.value
-//           cursor = await cursor.continue();
-//         }
-
-//         setFeedbacks(newFeedbacks)
-//       })
-//     }, [])
-
-//     return feedbacks
-//   }
+};
 
 const App = () => {
 
-
     return (
         <div className="app-div">
-
             <BrowserRouter>
                 <Navbar />
                 <Switch>
                     <Route path="/" exact>
                         <Users getStore={getStore} />
+                    </Route>
+                    <Route path="/my-feedback">
+                        <Feedbacks getStore={getStore} />
                     </Route>
                     <Route path='*'>
                         <div>404 not found</div>
