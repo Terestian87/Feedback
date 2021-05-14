14744e11053ec1ec4fc116e5ef9daf6f15d3c24f
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-05-14 18:03:34 +0200

    Corrected proper behaviour taking the value from the db and showing it accordingly // removed guideline borders and corrected classNames for styling

--- src/components/feedback/Scale.js
+++ src/components/feedback/Scale.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import '../../styles/feedback/scale.scss'
 
+const handleColor = (value, index) => {
+    if (!value) return ''
+    if (index <= value) {
+        if (value < 3) {
+            return 'red'
+        } else if (value < 7) {
+            return 'yellow'
+        } else if (value >= 7) {
+            return 'green'
+        } return ''
+    }
+}
+
+
+
 const Scale = ({ value }) => {
     //.map(currentValue, index)
-    const arr = [...Array(10)].map((e, i) => <div className="scale-box-value " key={i}></div>)
+    const arr = [...Array(10)].map((e, i) => <div className={`scale-box-value ${handleColor(value, i)}`} key={i}></div>)
     return <div className='box-container'>{arr}</div>
 }
 
